dc620ad8d3e18f03e65704cd25256ae420298720
Marlon <92185097+managaran@users.noreply.github.com>
2022-05-15 21:33:48 -1000

    Add files via upload

---
+++ dictionaries_lists.py
@@ -0,0 +1,67 @@
+# ! Update Values in Dictionaries and Lists
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+print(x)
+[0]['last_name'] = "Bryant"
+print(students)
+sports_directory['soccer'][0] = 'Andres'
+print( sports_directory['soccer'])
+z[0]['y'] = 30
+print(z)
+
+
+# ! Iterate Through a List of Dictionaries
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterate_dictionary(list):
+    for i in range(0, len(list)):
+        output = ""
+        for key,val in list[i].items():
+            output += f" {key} - {val},"
+        print(output)
+
+iterate_dictionary(students)
+
+# ! Get Values From a List of Dictionaries
+
+def iterate_dictionary2(key_name,list):
+    for i in range(0, len(list)):
+        
+        for key,val in list[i].items():
+            if key == key_name:
+                print(val)
+iterate_dictionary2('first_name',students)
+iterate_dictionary2('last_name',students)
+
+# ! Iterate Through a Dictionary with List Values
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def print_info(dict):
+    for key,val in dict.items():
+        print("--------------")
+        print(f"{len(val)} {key.upper()}")
+        for i in range(0, len(val)):
+            print(val[i])
+
+print_info(dojo)
